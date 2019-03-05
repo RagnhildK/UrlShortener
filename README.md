ba71079058aa2fee98590e4a954d65bde21771dd
Ragnhild Kleiven <ragnhildk98@gmail.com>
2019-03-05 17:11:01 +0100

    fghj

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # UrlShortener
-for å kjøre koden lokalt bruker man komandoen "python -m flask run --port=3000". På windows er det "py -m flask run --port=3000".
--- app.py
+++ app.py
@@ -7,19 +7,6 @@ app = Flask(__name__)
 url_mapping = {}
 
 
-def generate_short_url():
-    return "https://www.koble.jobs/"+''.join(random.choices(string.ascii_letters + string.digits, k=7)) #7 tilfeldige bokstaver/tall
-# kanskje legge inn ^ som en egen variabel - lettere å endre senere
-
-def get_long_url(url):
-    return url_mapping[url]
-
-
-def create_response_object(short_url, long_url):
-    response = {"shortUrl": short_url, "longUrl": long_url}
-    return jsonify(response)
-
-
 @app.route('/')
 def index():
     return render_template('base.html')
@@ -40,5 +27,20 @@ def expand_url():
     return create_response_object(short_url, long_url)
 
 
+# Har antatt at det er den samme prefiksen på alle de forkortede linkene og at siste del av linken er helt tilfeldig
+def generate_short_url():
+    prefix = "https://www.koble.jobs/"
+    return prefix +''.join(random.choices(string.ascii_letters + string.digits, k=7))
+
+
+def get_long_url(url):
+    return url_mapping[url]
+
+
+def create_response_object(short_url, long_url):
+    response = {"shortUrl": short_url, "longUrl": long_url}
+    return jsonify(response)
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run()
